ba169ff2d5a7bc0d41f97fdf12150f8f967ed016
HugoMazari <hugojrh@nevada.unr.edu>
2021-09-08 21:48:08 -0700

    Ensuring gitignore works and main file made.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------
+Name: main.cpp
+Summary: Entry point for program.
+Notes:
+
+     Name         Date      Version
+--------------  --------    -------
+Hugo Mazariego  09/08/21    1.0: Made File
+-----------------------------------------------------------*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+/// <summary>
+/// The program's entry point.
+/// </summary>
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
